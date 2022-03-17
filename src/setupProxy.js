2ce81f6af5afe8c2f3e6384c5676feb1e4a5bec8
Amit Singh <abcdamit44@gmail.com>
2022-03-17 09:42:35 +0530

    Trying to resolve the problem

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        '/api',
+        '/https://newsapi.org/v2/top-headlines?country=in&apiKey=5102124e3da246c9a8d06bd7ffffeb54',
         createProxyMiddleware({
             target: 'http://localhost:5000',
             changeOrigin: true,
